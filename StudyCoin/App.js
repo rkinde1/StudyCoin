db4e2495c97d248e5cacd8d38047047c8e86f5a4
rkinde1 <rosekinder2020@gmail.com>
2023-06-01 15:01:08 -0400

    Added Colors plus messed around

--- StudyCoin/App.js
+++ StudyCoin/App.js
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
+      <View style={{
+        backgroundColor: 'purple',
+        width: '100%',
+        height: 50,
+      }}>
+          <Text>Navbar</Text>
+      </View>
+      <View style = {{
+        flex: 4,
+      }}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Testing</Text>
+        <Button color="purple" title="Get started"></Button>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFE5B4',
     alignItems: 'center',
     justifyContent: 'center',
   },
